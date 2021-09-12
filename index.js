933a756d152e2cb28b45de7aafa0cee112c6685b
jendevelops <jennifer.batara.dev@gmail.com>
2021-09-11 22:26:14 -0700

    Make dark mode default

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 document.getElementById("dark-mode").addEventListener("change", (e) => {
     console.log(document.querySelectorAll("body *"));
     if (e.target.checked){
-        document.querySelector("label[for=dark-mode]").innerHTML="🌙";
-        document.querySelector("body").classList.add("dark");
-        // document.getElementsByClassName("main")[0].classList.add("dark")
-    } else{
         document.querySelector("label[for=dark-mode]").innerHTML="🌞";
         document.querySelector("body").classList.remove("dark");
-        // document.getElementsByClassName("main")[0].classList.remove("dark")
+    } else{
+        document.querySelector("label[for=dark-mode]").innerHTML="🌙";
+        document.querySelector("body").classList.add("dark");
     }
 })
